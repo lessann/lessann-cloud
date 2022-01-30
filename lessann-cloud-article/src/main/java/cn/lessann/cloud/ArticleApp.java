308ad3777f9124f8a2ec0ff5ad7a9a69690f4540
fishZH <jiangzhong91@gmail.com>
2022-01-30 17:46:56 +0800

    集成Redis

--- lessann-cloud-article/src/main/java/cn/lessann/cloud/ArticleApp.java
+++ lessann-cloud-article/src/main/java/cn/lessann/cloud/ArticleApp.java
@@ -1,4 +1,4 @@
-package cn.lessann.cloud.article;
+package cn.lessann.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
--- lessann-cloud-article/src/main/java/cn/lessann/cloud/article/controller/ArticleController.java
+++ lessann-cloud-article/src/main/java/cn/lessann/cloud/article/controller/ArticleController.java
@@ -1,9 +1,10 @@
 package cn.lessann.cloud.article.controller;
 
-import cn.lessann.cloud.article.feign.UserClient;
+import cn.lessann.cloud.api.user.UserApi;
 import cn.lessann.cloud.beans.http.Message;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +22,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class ArticleController {
 
     @Autowired
-    UserClient userClient;
+    UserApi userApi;
+
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
 
     @GetMapping("/{id}")
     @HystrixCommand(fallbackMethod = "errorResponse")
@@ -29,7 +33,13 @@ public class ArticleController {
         if (id == 1) {
             throw new RuntimeException("xxxx");
         }
-        return userClient.list();
+        return userApi.list();
+    }
+
+    @GetMapping("/sendRedis")
+    public String sendRedis() {
+        stringRedisTemplate.opsForValue().set("lessann", "lessann");
+        return "success";
     }
 
     public Message errorResponse(int id) {
